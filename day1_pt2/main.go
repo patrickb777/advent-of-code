7b6388470a13239b3712360b06509c560bbba1e9
Patrick B <patrick777@tuta.io>
2023-12-01 19:42:08 +0000

    Day1_pt2, unable to solve pt2

--- day1_pt2/main.go
+++ day1_pt2/main.go
@@ -46,14 +46,36 @@ func readFile(file string) Coordinates {
 }
 
 func getCoord(c Coordinates) Coordinates {
-	re := regexp.MustCompile("[0-9]")
+	re := regexp.MustCompile("(?:zero|one|two|three|four|five|six|seven|eight|nine|[0-9])") // overlapping strings are causing a problem, Golang doesn't support ?=
+	nConv := map[string]string{
+		"zero":  "0",
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
 	for _, v := range c.Input {
 		s := re.FindAllString(v, -1)
+		for i, n := range s {
+			mv, e := nConv[n]
+			if e {
+				s[i] = mv
+			}
+		}
 		coord, err := strconv.Atoi(s[0] + s[len(s)-1])
 		if err != nil {
 			log.Fatal(err)
 		}
 		c.Coord = append(c.Coord, coord)
 	}
+	for i, _ := range c.Coord {
+		log.Println(c.Input[i], " : ", c.Coord[i])
+	}
+
 	return c
 }
