7dafb9e382e3c9110353d50ac4b2fe26ef6ab364
Patrick Booth <patrick.booth@anaplan.com>
2023-12-04 01:14:19 +0000

    Day 3 pt2 prep

---
+++ day3_pt2/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"advent-of-code/readfile"
+	"flag"
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+type PartNumbers struct {
+	PartNo []int
+}
+
+type Numbers struct {
+	Metadata []NumMetadata
+}
+
+type NumMetadata struct {
+	Num   string
+	Len   int
+	Row   int
+	Pos   int
+	ProxX [2]int
+	ProxY [2]int
+}
+
+type Symbols struct {
+	Metadata []SymMetadata
+}
+
+type SymMetadata struct {
+	Char string
+	Row  int
+	Pos  int
+}
+
+func main() {
+	start := time.Now()
+	fmt.Println("Gear Ratios - Part Two")
+	f := flag.String("f", "none", "Input file")
+	flag.Parse()
+	inputFile := readfile.ReadFile(*f) // moved the reading of the input file to an external module
+	symbols, numbers := parseSchematic(inputFile)
+	partNos := getPartNumbers(symbols, numbers)
+	total := getSumOfParts(partNos)
+	fmt.Printf("Sum of Part Numbers = %d\n", total)
+
+	// Output execution time
+	elapsed := time.Since(start)
+	log.Printf("Execution time %s\n", elapsed)
+}
+
+func getSumOfParts(partNos PartNumbers) int {
+	total := 0
+	for _, n := range partNos.PartNo {
+		total = total + n
+	}
+	return total
+}
+
+func parseSchematic(input readfile.InputFile) (Symbols, Numbers) {
+	var symMetadata SymMetadata
+	var numMetadata NumMetadata
+	symbols := Symbols{}
+	numbers := Numbers{}
+	symRX := regexp.MustCompile(`[^a-zA-Z0-9\.]`)
+	numRX := regexp.MustCompile(`[0-9]+`)
+	for r, v := range input.InputRow {
+		// Find Symbols and Positions
+		runes := []rune(v)
+		for p, c := range runes {
+			sym := symRX.FindAllString(string(c), -1)
+			if len(sym) > 0 {
+				symMetadata.Char = sym[0]
+				symMetadata.Row = r
+				symMetadata.Pos = p
+				symbols.Metadata = append(symbols.Metadata, symMetadata)
+			}
+		}
+		// Find Numbers and Positions
+		num := numRX.FindAllString(v, -1)
+		numInd := numRX.FindAllStringIndex(v, -1)
+		//fmt.Println(v)
+		for i, n := range num {
+			numMetadata.Num = n
+			//fmt.Printf("Number:%s <<>> %s \n", numMetadata.Num, num[i])
+			//fmt.Printf("Number Index:%v \n", numInd[i][0])
+			numMetadata.Len = len(n)
+			numMetadata.Row = r
+			numMetadata.Pos = numInd[i][0]
+			numMetadata.ProxX[0] = numMetadata.Pos - 1
+			numMetadata.ProxX[1] = numMetadata.Pos + numMetadata.Len
+			numMetadata.ProxY[0] = r - 1
+			numMetadata.ProxY[1] = r + 1
+			numbers.Metadata = append(numbers.Metadata, numMetadata)
+		}
+	}
+	return symbols, numbers
+}
+
+func getPartNumbers(s Symbols, n Numbers) PartNumbers {
+	var partNos PartNumbers
+	for _, nmd := range n.Metadata {
+		//fmt.Printf("Number:%s:, Length:%d, Postion:%d, X Range:%d:%d, Y Range:%d:%d \n", nmd.Num, nmd.Len, nmd.Pos, nmd.ProxX[0], nmd.ProxX[1], nmd.ProxY[0], nmd.ProxY[1])
+		for _, smd := range s.Metadata {
+			if smd.Row >= nmd.ProxY[0] && smd.Row <= nmd.ProxY[1] && smd.Pos >= nmd.ProxX[0] && smd.Pos <= nmd.ProxX[1] {
+				//log.Printf("R:%d, N:%s, Proximity of S:%s \n", nmd.Row, nmd.Num, smd.Char)
+				p, err := strconv.Atoi(nmd.Num)
+				if err != nil {
+					log.Fatal(err)
+				}
+				partNos.PartNo = append(partNos.PartNo, p)
+
+			}
+		}
+	}
+	return partNos
+}
---
+++ day3_pt2/main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"advent-of-code/readfile"
+	"testing"
+)
+
+func TestReadfile(t *testing.T) {
+	inputFile := readfile.ReadFile("engine-test-1.txt")
+	if len(inputFile.InputRow) != 10 {
+		t.Errorf("Read of input file was incorrect, got: %d, want: %d.", len(inputFile.InputRow), 4)
+	}
+}
+
+func TestFullResult1(t *testing.T) {
+	inputFile := readfile.ReadFile("engine-test-1.txt")
+	symbols, numbers := parseSchematic(inputFile)
+	partNos := getPartNumbers(symbols, numbers)
+	total := getSumOfParts(partNos)
+	if total != 4361 {
+		t.Errorf("Sum of parts calculation incorrect, got: %d, want: %d.", total, 4361)
+	}
+}
+
+func TestFullResult2(t *testing.T) {
+	inputFile := readfile.ReadFile("engine-test-2.txt")
+	symbols, numbers := parseSchematic(inputFile)
+	partNos := getPartNumbers(symbols, numbers)
+	total := getSumOfParts(partNos)
+	if total != 413 {
+		t.Errorf("Sum of parts calculation incorrect, got: %d, want: %d.", total, 413)
+	}
+}
+
+func TestFullResult3(t *testing.T) {
+	inputFile := readfile.ReadFile("engine-test-3.txt")
+	symbols, numbers := parseSchematic(inputFile)
+	partNos := getPartNumbers(symbols, numbers)
+	total := getSumOfParts(partNos)
+	if total != 925 {
+		t.Errorf("Sum of parts calculation incorrect, got: %d, want: %d.", total, 925)
+	}
+}
+
+func TestFullResult4(t *testing.T) {
+	inputFile := readfile.ReadFile("engine-test-4.txt")
+	symbols, numbers := parseSchematic(inputFile)
+	partNos := getPartNumbers(symbols, numbers)
+	total := getSumOfParts(partNos)
+	if total != 8082 {
+		t.Errorf("Sum of parts calculation incorrect, got: %d, want: %d.", total, 8082)
+	}
+}
+
+func TestFullResult5(t *testing.T) {
+	inputFile := readfile.ReadFile("engine-test-5.txt")
+	symbols, numbers := parseSchematic(inputFile)
+	partNos := getPartNumbers(symbols, numbers)
+	total := getSumOfParts(partNos)
+	if total != 1931 {
+		t.Errorf("Sum of parts calculation incorrect, got: %d, want: %d.", total, 1931)
+	}
+}
