c26c77f5fbd089608632088cc0a9114e656d99bb
Patrick Booth <patrick.booth@anaplan.com>
2023-12-08 12:34:36 +0000

    Day 8 completed

--- day8/main.go
+++ day8/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -17,11 +18,58 @@ func main() {
 
 	// Parse Input
 	inputFile := readfile.ReadFile(*f)
-	fmt.Println(inputFile)
+	//fmt.Println(inputFile)
+	directions, navigation := parseDirections(inputFile)
+	fmt.Println(directions)
+	fmt.Println(navigation)
 
 	// Processing
+	position := "AAA"
+	endFlag := 0
+	pedometer := 0
+	for {
+		for d := 0; d < len(directions); d++ {
+			fmt.Println("Current Position:", position)
+			switch directions[d] {
+			case 76: // left
+				position = navigation[position][0]
+			case 82: // right
+				position = navigation[position][1]
+			}
+			pedometer++
+			fmt.Println("Moving to:", position)
+			if position == "ZZZ" {
+				endFlag = 1
+			}
+		}
+		if endFlag == 1 {
+			break
+		}
+	}
+
+	fmt.Printf("Total steps taken: %d\n", pedometer)
 
 	// Output execution time
 	elapsed := time.Since(start)
 	log.Printf("Execution time %s\n", elapsed)
+}
+
+func parseDirections(input readfile.InputFile) ([]rune, map[string][2]string) {
+	// rune 76 == L
+	// rune 82 == R
+	var directions []rune
+	nav := make(map[string][2]string)
+	for _, d := range input.InputRow[0] {
+		directions = append(directions, d)
+	}
+	for i, row := range input.InputRow {
+		if i >= 2 {
+			splitStr := strings.Split(row, " = ")
+			key := splitStr[0]
+			v := strings.Split(strings.TrimSuffix(strings.TrimPrefix(splitStr[1], "("), ")"), ", ") // trim leading "(", then trailing ")", then split on ", "
+			val := [2]string{v[0], v[1]}
+			nav[key] = val
+		}
+	}
+	return directions, nav
 }
